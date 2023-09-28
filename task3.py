4c4e8444de686663a390e88d0cde05ec1a314246
Hugo Valle <hugovalle1@weber.edu>
2023-09-28 00:15:15 +0000

    Complete task 3

--- task3.py
+++ task3.py
@@ -9,24 +9,41 @@ def tier_water_bill():
     # Task 1: Basic Water Usage
     # Take user input for the number of gallons of water used in a household
     gallons_usage = int(input('How many gallons you used this month? '))
-
+    
+    charge_tier1 = charge_tier2 = charge_tier3 = 0.00
+    gallons_tier1 = gallons_tier2 = gallons_tier3 = 0
     # Basic, minimum charge
     if gallons_usage <= 2000:
-        final_charge = 30.00
+        charge_tier1 = 30.00
+        gallons_tier1 = gallons_usage
     else:
         # Tier 2: 2001 - 5000 gallons
         # Gallons in this range are charged at $0.08 per gallon
         if gallons_usage > 2000 and gallons_usage <= 5000:
-            gallons_tier2 = gallons_usage - 2000
-            final_charge = (gallons_tier2 * 0.08) + 30.00
+            # Tier 1: 2000 gallons
+            charge_tier1 = 30.00
+            gallons_tier1 = 2000
+            # Tier 2: 2001 - 5000 gallons
+            gallons_tier2 = gallons_usage - gallons_tier1
+            charge_tier2 = gallons_tier2 * 0.08
         # Tier 3: more than 5000 gallons
         # Gallons in this range are charged at $0.20 per gallon
         else:
-            gallons_tier2 = gallons_usage - 2000
-            gallons_tier3 = gallons_tier2 - 3000
-            final_charge = (gallons_tier2 * 0.08) + (gallons_tier3 * 0.20) + 30.00
+            # Tier 1: 2000 gallons
+            charge_tier1 = 30.00
+            gallons_tier1 = 2000
+            # Tier 2: 2001 - 5000 gallons
+            gallons_tier2 = 3000
+            charge_tier2 = gallons_tier2 * 0.08
+            # Tier 3: more than 5000 gallons 
+            gallons_tier3 = gallons_usage - (gallons_tier1 + gallons_tier2)
+            charge_tier3 = gallons_tier3 * 0.20
 
+    final_charge = charge_tier1 + charge_tier2 + charge_tier3
     print(f'You used {gallons_usage} gallons, your bill is ${final_charge:.2f}')
+    print(f'Tier1 (0-2000): {gallons_tier1} gallons,  ${charge_tier1:.2f}')
+    print(f'Tier2 (2001-5000): {gallons_tier2} gallons,  ${charge_tier2:.2f}')
+    print(f'Tier3 (more than 5000): {gallons_tier3} gallons,  ${charge_tier3:.2f}')
 
 
 if __name__ == '__main__':
--- test_task3.py
+++ test_task3.py
@@ -9,12 +9,12 @@ from task3 import tier_water_bill
 # First element in the tuple is the input, second element is the
 #  expected output
 @pytest.mark.parametrize("gallons_usage, expected_output", [
-    (100, "You used 100 gallons, your bill is $30.00\n"),
-    (2000, "You used 2000 gallons, your bill is $30.00\n"),
-    (2001, "You used 2001 gallons, your bill is $30.08\n"),
-    (4520, "You used 4520 gallons, your bill is $231.60\n"),
-    (5000, "You used 5000 gallons, your bill is $270.00\n"),
-    (5020, "You used 5020 gallons, your bill is $275.60\n"),
+    (100, "You used 100 gallons, your bill is $30.00\nTier1 (0-2000): 100 gallons,  $30.00\nTier2 (2001-5000): 0 gallons,  $0.00\nTier3 (more than 5000): 0 gallons,  $0.00\n"),
+    (2000, "You used 2000 gallons, your bill is $30.00\nTier1 (0-2000): 2000 gallons,  $30.00\nTier2 (2001-5000): 0 gallons,  $0.00\nTier3 (more than 5000): 0 gallons,  $0.00\n"),
+    (2001, "You used 2001 gallons, your bill is $30.08\nTier1 (0-2000): 2000 gallons,  $30.00\nTier2 (2001-5000): 1 gallons,  $0.08\nTier3 (more than 5000): 0 gallons,  $0.00\n"),
+    (4520, "You used 4520 gallons, your bill is $231.60\nTier1 (0-2000): 2000 gallons,  $30.00\nTier2 (2001-5000): 2520 gallons,  $201.60\nTier3 (more than 5000): 0 gallons,  $0.00\n"),
+    (5000, "You used 5000 gallons, your bill is $270.00\nTier1 (0-2000): 2000 gallons,  $30.00\nTier2 (2001-5000): 3000 gallons,  $240.00\nTier3 (more than 5000): 0 gallons,  $0.00\n"),
+    (5020, "You used 5020 gallons, your bill is $274.00\nTier1 (0-2000): 2000 gallons,  $30.00\nTier2 (2001-5000): 3000 gallons,  $240.00\nTier3 (more than 5000): 20 gallons,  $4.00\n"),
 ])
 
 
